d3eba14adade3649eff2f25dd597ad5e24642a5d
김은혜 <jailke93@gmail.com>
2021-02-16 19:09:05 +0900

    js: handle listening

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+function handleListening() {
+  console.log("I'm Listening");
+}
+
+app.listen(port, handleListening);
